efc993e8a3fe13a9385e9b5fd29813c8dbe41bb2
Cenk Alti <cenkalti@gmail.com>
2015-01-27 15:37:19 +0200

    fix package

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ from setuptools import setup
 
 setup(
     name='Kuyruk-Manager',
-    version="1.0.1",
+    version="1.0.4",
     author=u'Cenk Altı',
     author_email='cenkalti@gmail.com',
     keywords='kuyruk manager',
     url='https://github.com/cenkalti/kuyruk-manager',
-    py_modules=["kuyruk_manager"],
+    packages=["kuyruk_manager"],
+    include_package_data=True,
     install_requires=[
         'kuyruk>=3.0.0',
         'redis>=2.10',
         'Flask>=0.10',
         'rpyc>=3.3',
     ],
-    entry_points={'kuyruk.config': 'manager = kuyruk_manager:CONFIG',
-                  'kuyruk.commands': 'manager = kuyruk_manager:command'},
+    entry_points={'kuyruk.config': 'manager = kuyruk_manager.__init__:CONFIG',
+                  'kuyruk.commands': 'manager = kuyruk_manager:__init__.command'},
     description='Manage Kuyruk workers.',
     long_description=open('README.md').read(),
 )
